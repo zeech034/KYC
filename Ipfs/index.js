178def9b393512e3ef1e8b0f4c13079f943c55e6
kimzaTech <kimzatech@gmail.com>
2022-09-09 22:37:05 +0300

    ipfs

---
+++ Ipfs/index.js
@@ -0,0 +1,5 @@
+const ipfsClient = require('ipfs-http-client');
+
+const ipfs = ipfsClient('http://localhost:5001'); // connecting to the daemon API server
+
+
---
+++ Ipfs/test.js
@@ -0,0 +1,6 @@
+import * as IPFS from 'ipfs-core'
+
+const ipfs = await IPFS.create()
+const { cid } = await ipfs.add('Hello world')
+console.info(cid)
+// QmXXY5ZxbtuYj6DnfApLiGstzPN7fvSyigrRee3hDWPCaf
